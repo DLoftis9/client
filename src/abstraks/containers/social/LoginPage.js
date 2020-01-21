5de339a0d585b7ea55ff8c6104d33d27c23dbe75
Darius Loftis <dloft9@gmail.com>
2020-01-20 17:18:30 -0800

    removed material-ui classes and components, rearranged props and methods. added class names to html elements

--- src/abstraks/containers/social/LoginPage.js
+++ src/abstraks/containers/social/LoginPage.js
@@ -4,22 +4,10 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import compose from 'recompose/compose';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import LockIcon from '@material-ui/icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
-
 import { loginUser } from '../../../base/social/actions/authActions';
 
 const propTypes = {
   containerName: PropTypes.string.isRequired,
-  classes: PropTypes.object.isRequired,
   errors: PropTypes.object,
   history: PropTypes.object.isRequired,
   signInUser: PropTypes.func.isRequired,
@@ -30,48 +18,13 @@ const defaultProps = {
   errors: {},
 };
 
-const styles = theme => ({
-  layout: {
-    width: 'auto',
-    display: 'block', // Fix IE11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    padding: theme.spacing.unit * 4,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: theme.spacing.unit * 50,
-    backgroundColor: 'theme.palette.background.paper',
-    boxShadow: theme.shadows[5],
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-  link: {
-    textDecoration: 'none',
-  },
-  footer: {
-    marginTop: theme.spacing.unit * 2,
-  },
-  errorText: {
-    color: '#D50000',
-    marginTop: '5px',
-  },
+const mapStateToProps = state => ({
+  auth: state.authReducer,
+  errors: state.errorReducer,
+});
+
+const mapDispatchToProps = dispatch => ({
+  signInUser: user => dispatch(loginUser(user)),
 });
 
 class LoginPage extends Component {
@@ -118,81 +71,68 @@ class LoginPage extends Component {
   };
 
   render() {
-    const { classes } = this.props;
     const { errors } = this.state;
 
     return (
-      <React.Fragment>
-        <CssBaseline />
-        <main className={classes.layout}>
-          <Paper className={classes.paper}>
-            <Avatar className={classes.avatar}>
-              <LockIcon />
-            </Avatar>
-            <Typography variant="headline">Log In</Typography>
-            <form onSubmit={this.handleSubmit} noValidate>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="email">Email Address</InputLabel>
-                <Input
-                  onChange={this.handleInputChange}
-                  id="email"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                  error={!!errors.email}
-                />
-                <span className={classes.errorText}>{errors.email}</span>
-              </FormControl>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <Input
-                  onChange={this.handleInputChange}
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  error={!!errors.password}
-                />
-                <span className={classes.errorText}>{errors.password}</span>
-              </FormControl>
+      <main className={this.props.containerName}>
+        <div className="login_form">
+          <div className="icon_wrapper">
+            <p className="icon">Sign Up Icon</p>
+          </div>
+          <h3 className="header-3">Log In</h3>
+          <form className="form" onSubmit={this.handleSubmit} noValidate>
+            <div className="form-control" required>
+              <label className="label" htmlFor="email">
+                Email Address
+              </label>
+              <input
+                className="input-text"
+                onChange={this.handleInputChange}
+                id="email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                error={!!errors.email}
+              />
+              <span className="error_text">{errors.email}</span>
+            </div>
 
-              <Button
-                type="submit"
-                fullWidth
-                variant="raised"
-                color="primary"
-                className={classes.submit}
-              >
-                Log In
-              </Button>
-            </form>
-            <Typography className={classes.footer} variant="body1">
-              {"Don't have an account? "}
-              <NavLink to="/signup" className={classes.link}>
-                Sign Up
-              </NavLink>
-            </Typography>
-          </Paper>
-        </main>
-      </React.Fragment>
+            <div className="form-control" required>
+              <label className="label" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="input-text"
+                onChange={this.handleInputChange}
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                error={!!errors.password}
+              />
+              <span className="error_text">{errors.password}</span>
+            </div>
+
+            <button className="button submit" type="submit">
+              Log In
+            </button>
+          </form>
+          <h3 className="reroute">
+            {"Don't have an account? "}
+            <NavLink to="/signup" className="reroute_link">
+              Sign Up
+            </NavLink>
+          </h3>
+        </div>
+      </main>
     );
   }
 }
-
-const mapStateToProps = state => ({
-  auth: state.authReducer,
-  errors: state.errorReducer,
-});
-
-const mapDispatchToProps = dispatch => ({
-  signInUser: user => dispatch(loginUser(user)),
-});
 
 LoginPage.propTypes = propTypes;
 LoginPage.defaultProps = defaultProps;
 
 export default compose(
-  withStyles(styles),
   connect(
     mapStateToProps,
     mapDispatchToProps,
