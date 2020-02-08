cd087ee721c73fb7aa6b9a73e7e1baf5961d9265
Darius Loftis <dloft9@gmail.com>
2020-02-07 17:57:52 -0800

    added notes for logic, cleaned code

--- src/base/social/utils/Context.js
+++ src/base/social/utils/Context.js
@@ -4,10 +4,23 @@ import Data from './Data';
 
 const Context = React.createContext();
 
-// A higher-order component (HOC) that shares functionality across 
-// the components of the app. This allows reusability for component logic 
-// and state. "Context" is used in React when data needs to 
+// A higher-order component (HOC) that shares functionality across
+// the components of the app. This allows reusability for component logic
+// and state. "Context" is used in React when data needs to
 // be accessible by many components at different nesting levels.
+
+// Context is primarily used when some data needs to be accessible by many
+// components at different nesting levels. Context lets you pass data
+// through the component tree without having to pass props down manually at
+// every level.
+
+// When using the Context API, the Provider is what provides the
+// data that needs to be consumed by other components of the application.
+
+// The Provider class in the file Context.js is a "higher-order component"
+// that returns a Provider component which provides the application state and
+// any actions or event handlers that need to be shared between components,
+// via a required value prop.
 export class Provider extends Component {
   state = {
     authenticatedUser: Cookies.getJSON('authenticatedUser') || null,
@@ -28,6 +41,9 @@ export class Provider extends Component {
         signOut: this.signOut,
       },
     };
+
+    // value represents an object containing the context to be shared
+    // throughout the component tree.
     return <Context.Provider value={value}>{this.props.children}</Context.Provider>;
   }
 
@@ -39,11 +55,11 @@ export class Provider extends Component {
           authenticatedUser: user,
         };
       });
-      const cookieOptions = {
-        // 1 day
-        expires: 1, 
-      };
-      Cookies.set('authenticatedUser', JSON.stringify(user), { cookieOptions });
+      // const cookieOptions = {
+      //   // 1 day
+      //   expires: 1,
+      // };
+      // Cookies.set('authenticatedUser', JSON.stringify(user), { cookieOptions });
     }
     return user;
   };
@@ -57,11 +73,13 @@ export class Provider extends Component {
 export const Consumer = Context.Consumer;
 
 /**
- * A higher-order component that wraps the provided component in a Context Consumer component.
  * @param {class} Component - A React component.
  * @returns {function} A higher-order component.
  */
 
+// withContext is a higher-order componen that wraps a provided component in
+// a <Context.Consumer> component. withContext automatically subscribes
+// (or connects) the component passed to it to all actions and context changes:
 export default function withContext(Component) {
   return function ContextComponent(props) {
     return (
