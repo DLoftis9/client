42fb197309680f1d1c676c19afb26fb96fb6318f
Darius Loftis <dloft9@gmail.com>
2020-02-10 11:31:57 -0800

    cleaned code

--- src/abstraks/components/social/Form.js
+++ src/abstraks/components/social/Form.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 // Form used for UserSignUp and UserSignIn containers
 
-// A function that renders any validation errors sent from the API, 
-// via the <ErrorsDisplay> function component. It also renders the 
-// "Submit" button of a form, as well as handling it's functionality, 
-// via the functions handleSubmit. Props are passed 
-// to this component – from a parent component like UserSignUp – to 
+// A function that renders any validation errors sent from the API,
+// via the <ErrorsDisplay> function component. It also renders the
+// "Submit" button of a form, as well as handling it's functionality,
+// via the functions handleSubmit. Props are passed
+// to this component – from a parent component like UserSignUp – to
 // provide it the data it needs.
 export default props => {
   const { cancel, errors, submit, submitButtonText, elements } = props;
