f86e5ff7afd69c991185812ef29b8706f8437b70
Darius Loftis <dloft9@gmail.com>
2020-02-11 16:15:25 -0800

    created new components, markup for post-feed page

--- src/abstraks/components/social/PostResponse.js
+++ src/abstraks/components/social/PostResponse.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import PostResponseComments from './PostResponseComments';
+
 const propTypes = {
   componentName: PropTypes.string.isRequired,
 };
@@ -10,31 +12,37 @@ const defaultProps = {
 };
 
 const PostResponse = props => (
-  <div className={props.componentName}>
-    <div className={props.componentName + `_container`}>
-      <div className="avatar">
-        <div className="avatar-image">
-          <i class="fa fa-user" aria-hidden="true"></i>
+  <React.Fragment>
+    <div className={props.componentName}>
+      <div className={props.componentName + `_container`}>
+        <div className="avatar">
+          <div className="avatar-image">
+            <i class="fa fa-user" aria-hidden="true"></i>
+          </div>
+          <div className="avatar-name">
+            <p className="user-name">User Name</p>
+          </div>
+          <div className="avatar-time">
+            <p className="post-timestamp paragraph">Mon Dec 16 2019</p>
+          </div>
         </div>
-        <div className="avatar-name">
-          <p className="user-name">User Name</p>
+        <div className="post">
+          <p className="post-paragraph paragraph">
+            Uniquely cultivate premium supply chains after world-class leadership skills. Completely
+            implement installed base models with global e-markets. Efficiently foster high-quality
+            bandwidth and strategic value. Globally.
+          </p>
+          <i class="fa fa-heart" aria-hidden="true">
+            <p className="post-heart-number">9</p>
+          </i>
+          <i class="fa fa-comment-o" aria-hidden="true">
+            <p className="post-comment-number">11</p>
+          </i>
         </div>
-      </div>
-      <div className="post">
-        <p>
-          Uniquely cultivate premium supply chains after world-class leadership skills. Completely
-          implement installed base models with global e-markets. Efficiently foster high-quality
-          bandwidth and strategic value. Globally.
-        </p>
-        <i class="fa fa-heart" aria-hidden="true">
-          <p className="post-heart-number">9</p>
-        </i>
-        <i class="fa fa-comment-o" aria-hidden="true">
-          <p className="post-comment-number">11</p>
-        </i>
       </div>
     </div>
-  </div>
+    <PostResponseComments />
+  </React.Fragment>
 );
 
 PostResponse.propTypes = propTypes;
