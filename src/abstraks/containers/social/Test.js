12fae9a6ceef7dc8f628a25d8c7e4067fedf1c56
Darius Loftis <dloft9@gmail.com>
2020-02-14 16:03:05 -0800

    cleaned code, css for menu in header, updated editInput components logic and markup

--- src/abstraks/containers/social/Test.js
+++ src/abstraks/containers/social/Test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Header from '../../components/testComponents/Header';
-import MainContent from '../../components/testComponents/MainContent';
+import EditInputElement from '../../../base/scripts/EditInputElement';
 
 export default class Test extends React.PureComponent {
   static propTypes = {
@@ -19,103 +18,14 @@ export default class Test extends React.PureComponent {
     guests: [],
   };
 
-  lastGuestId = 0;
-
-  newGuestId = () => {
-    const id = this.lastGuestId;
-    this.lastGuestId += 1;
-    return id;
-  };
-
-  toggleGuestProperty = (property, id) =>
-    this.setState({
-      guests: this.state.guests.map(guest => {
-        if (id === guest.id) {
-          return {
-            ...guest,
-            [property]: !guest[property],
-          };
-        }
-        return guest;
-      }),
-    });
-
-  toggleConfirmation = id => this.toggleGuestProperty('isConfirmed', id);
-
-  removeGuest = id =>
-    this.setState({
-      guests: this.state.guests.filter(guest => id !== guest.id),
-    });
-
-  toggleEditing = id => this.toggleGuestProperty('isEditing', id);
-
-  setName = (name, id) =>
-    this.setState({
-      guests: this.state.guests.map(guest => {
-        if (id === guest.id) {
-          return {
-            ...guest,
-            name,
-          };
-        }
-        return guest;
-      }),
-    });
-
-  toggleFilter = () => this.setState({ isFiltered: !this.state.isFiltered });
-
-  handleNameInput = e => this.setState({ pendingGuest: e.target.value });
-
-  newGuestSubmitHandler = e => {
-    e.preventDefault();
-    const id = this.newGuestId();
-    this.setState({
-      guests: [
-        {
-          name: this.state.pendingGuest,
-          isConfirmed: false,
-          isEditing: false,
-          id,
-        },
-        ...this.state.guests,
-      ],
-      pendingGuest: '',
-    });
-  };
-
-  getTotalInvited = () => this.state.guests.length;
-
-  getAttendingGuests = () =>
-    this.state.guests.reduce((total, guest) => (guest.isConfirmed ? total + 1 : total), 0);
-
   render() {
-    const totalInvited = this.getTotalInvited();
-    const numberAttending = this.getAttendingGuests();
-    const numberUnconfirmed = totalInvited - numberAttending;
     const { containerName } = this.props;
     return (
       <div className={containerName}>
         <div className={containerName + ` container`}>
           <div className={containerName + ` row`}>
             <h1>Test page</h1>
-            <Header
-              newGuestSubmitHandler={this.newGuestSubmitHandler}
-              pendingGuest={this.state.pendingGuest}
-              handleNameInput={this.handleNameInput}
-            />
-            <MainContent
-              toggleFilter={this.toggleFilter}
-              isFiltered={this.state.isFiltered}
-              totalInvited={totalInvited}
-              numberAttending={numberAttending}
-              numberUnconfirmed={numberUnconfirmed}
-              guests={this.state.guests}
-              toggleConfirmation={this.toggleConfirmation}
-              toggleEditing={this.toggleEditing}
-              setName={this.setName}
-              removeGuest={this.removeGuest}
-              pendingGuest={this.state.pendingGuest}
-            />
+            <EditInputElement />
           </div>
         </div>
       </div>
---
+++ src/base/scripts/EditInputElement.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class EditInputElement extends Component {
+  render() {
+    return (
+      <>
+        <span>Just a span</span>
+      </>
+    );
+  }
+}
