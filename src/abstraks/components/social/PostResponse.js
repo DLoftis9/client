7a3a2315f81fbb42b6330758559bb23c68331888
Darius Loftis <dloft9@gmail.com>
2020-02-26 14:41:44 -0800

    markup and organization for components, css for comment and like icons

--- src/abstraks/components/social/PostResponse.js
+++ src/abstraks/components/social/PostResponse.js
@@ -15,29 +15,31 @@ const PostResponse = props => (
       <div className={props.componentName + `_container`}>
         <div className="avatar">
           <div className="avatar-image">
-            <i className="fa fa-user" aria-hidden="true"></i>
+            <i className="image fa fa-user" aria-hidden="true"></i>
           </div>
           <div className="avatar-name">
-            <p className="user-name">User Name</p>
+            <p className="user-name">Posted User</p>
           </div>
         </div>
         <div className="post">
           <div className="post-image">
-            <i class="fa fa-picture-o" aria-hidden="true"></i>
+            <i class="image fa fa-picture-o" aria-hidden="true"></i>
           </div>
           <p className="post-paragraph paragraph">
             Uniquely cultivate premium supply chains after world-class leadership skills. Completely
             implement installed base models with global e-markets. Efficiently foster high-quality
             bandwidth and strategic value. Globally.
           </p>
-          <i className="fa fa-heart" aria-hidden="true">
-            <p className="post-heart-number">9</p>
-          </i>
-          <i className="fa fa-comment-o" aria-hidden="true">
-            <p className="post-comment-number">11</p>
-          </i>
-          <div className="post-time">
-            <p className="post-timestamp paragraph">Mon Dec 16 2019</p>
+          <div className="response-info">
+            <i className="fa fa-heart" aria-hidden="true">
+              <p className="post-heart-number">9</p>
+            </i>
+            <i className="fa fa-comment-o" aria-hidden="true">
+              <p className="post-comment-number">11</p>
+            </i>
+            <div className="post-time">
+              <p className="post-timestamp paragraph">Mon Dec 16 2019</p>
+            </div>
           </div>
         </div>
       </div>
