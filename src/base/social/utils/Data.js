cb1ea1a4617a9e7a28f85c82269a67dfd69c099e
Darius Loftis <dloft9@gmail.com>
2020-03-17 16:19:22 -0700

    debug to receive data from api

--- src/base/social/utils/Data.js
+++ src/base/social/utils/Data.js
@@ -78,7 +78,7 @@ export default class Data {
   async createUser(user) {
     const response = await this.api('/users', 'POST', user);
     if (response.status === 201) {
-      return [];
+      return [] ;
     } else if (response.status === 400) {
       return response.json().then(data => {
         return data.errors;
